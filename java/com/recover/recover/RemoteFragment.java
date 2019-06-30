15a47c6599ed1df20a7dc676757f99ffb2cb64ff
Manuel4131 <Manual131419@gmail.com>
2019-06-30 14:11:45 +0800

    Add description to app

--- java/com/recover/recover/RemoteFragment.java
+++ java/com/recover/recover/RemoteFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
 import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 public class RemoteFragment extends Fragment {
     @Nullable
@@ -17,6 +23,33 @@ public class RemoteFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.favorite_fragment,container,false);
         TextView selfIntroView = view.findViewById(R.id.self_intro);
+        readAppIntroduction(selfIntroView);
         return view;
+    }
+
+    private void readAppIntroduction(TextView textView){
+        String passage = "";
+        String text="";
+        try {
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(getContext().getAssets().open("introduction")));
+            while((text=reader.readLine())!=null){
+                if(text.startsWith("About"))
+                    text = text +'\n';
+
+                if(text.startsWith("When the"))
+                    text = '\n'+'\n' + text;
+                passage = passage + text;
+//                if(text.startsWith("Any advice")|text.startsWith("Contact"))
+//                    text = '\n'+ text;
+//                contactTV.
+            }
+            textView.setText(passage);
+//            reader.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
